948424475e4baa71963163528bdaf262e8e83b88
Nicolas <nicolas.connan@gmail.com>
2022-03-04 12:13:43 -0500

    Création de plusieurs fichiers par ressource

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -3,3 +3,8 @@ output "BastionHost_public_ip" {
   description = "Bastion Host Public IP"
   value = aws_instance.BastionHost.public_ip
 }
+
+output "VPC_id" {
+  description = "VPC Id"
+  value = aws_vpc.vpc_prod.id
+}
